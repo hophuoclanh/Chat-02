b703d3385c1032ac47d08eec3e6053e53ea82558
hophuoclanh <hophuoclanh@gmail.com>
2024-06-08 03:07:25 +0700

    chat

--- chat.js
+++ chat.js
@@ -71,17 +71,25 @@ $(document).ready(function() {
         var messages = document.getElementById('messages');
         var msgDiv = document.createElement('div');
         msgDiv.className = type === 'incoming' ? 'd-flex justify-content-start mb-4' : 'd-flex justify-content-end mb-4';
-
+    
         msgDiv.innerHTML = `
             <div class="img_cont_msg">
                 <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
             </div>
             <div class="msg_cotainer${type === 'incoming' ? '' : '_send'}">
+                <span class="msg_user">${user}</span>
                 ${msg}
                 <span class="msg_time${type === 'incoming' ? '' : '_send'}">${new Date().toLocaleTimeString()}, Today</span>
             </div>`;
         messages.appendChild(msgDiv);
     }
+    
+
+    function clearMessages() {
+        var messages = document.getElementById('messages');
+        messages.innerHTML = '';
+    }
+
 
     document.getElementById('send').addEventListener('click', function() {
         sendMessage();
@@ -101,12 +109,13 @@ $(document).ready(function() {
                 type: 'GET',
                 data: { query: query },
                 success: function(response) {
+                    console.log('Search users response:', response); // Debugging log
                     displaySearchResults(response);
                 },
                 error: function(xhr, status, error) {
-                    console.error('Search failed', xhr.responseJSON.message);
+                    console.error('Search failed', xhr.responseJSON ? xhr.responseJSON.message : error);
                 }
-            });
+            });            
         } else {
             $('#searchResults').empty();
         }
@@ -129,6 +138,58 @@ $(document).ready(function() {
             var userName = $(this).data('name');
             console.log(`User ${userName} clicked for private chat.`);
             initiatePrivateChat(userName);
+            
+            // Hide search results and clear the search input
+            $('#searchInput').val('');
+            $('#searchResults').empty();
+        });
+    }
+
+    $('#searchUserInput').on('input', function() {
+        var query = $(this).val();
+        if (query.trim() !== '') {
+            $.ajax({
+                url: 'http://localhost:5000/search_users',
+                type: 'GET',
+                data: { query: query },
+                success: function(response) {
+                    console.log('Search users response:', response); // Debugging log
+                    displayUserSearchResults(response);
+                },
+                error: function(xhr, status, error) {
+                    console.error('Search failed', xhr.responseJSON ? xhr.responseJSON.message : error);
+                }
+            });
+        } else {
+            $('#userSearchResults').empty();
+        }
+    });
+
+    function displayUserSearchResults(users) {
+        var results = $('#userSearchResults');
+        results.empty();
+        users.forEach(user => {
+            var userItem = `<li class="d-flex bd-highlight user-search-result" data-name="${user.name}">
+                                <div class="user_info">
+                                    <span>${user.name}</span>
+                                    <p>${user.phone}</p>
+                                </div>
+                            </li>`;
+            results.append(userItem);
+        });
+
+        $('.user-search-result').click(function() {
+            var userName = $(this).data('name');
+            console.log(`User ${userName} clicked for adding to group.`);
+            var currentUsers = $('#groupUsers').val().split(',').map(user => user.trim());
+            if (!currentUsers.includes(userName)) {
+                currentUsers.push(userName);
+                $('#groupUsers').val(currentUsers.join(', '));
+            }
+            
+            // Hide search results and clear the search input
+            $('#searchUserInput').val('');
+            $('#userSearchResults').empty();
         });
     }
 
@@ -137,41 +198,44 @@ $(document).ready(function() {
     }
 
     function initiatePrivateChat(userName) {
+        clearMessages();  // Clear previous messages
         currentChatUser = userName;
         $('#chatRoomName').text(userName);
         var room = generateRoomName(localUserName, userName);
-
+    
         console.log(`Initiating private chat with ${userName}. Room: ${room}`);
         console.log(`Current chat user set to ${currentChatUser}`);
         console.log(`Local user: ${localUserName}`);
         currentRoom = room;
-
+    
         socket.send(JSON.stringify({ action: 'create_room', room: room }));
         socket.send(JSON.stringify({ action: 'join', username: localUserName, room: room }));
     }
 
     function joinRoom(roomName) {
-        currentRoom = roomName; // Set the current chat room to the selected room
-        currentChatUser = null; // Invalidate the currentChatUser for group chat
+        clearMessages();  // Clear previous messages
+        currentRoom = roomName;  // Set the current chat room to the selected room
+        currentChatUser = null;  // Invalidate the currentChatUser for group chat
         $('#chatRoomName').text(roomName);
-
+    
         console.log(`Joining room: ${roomName}`);
         console.log(`Local user: ${localUserName}`);
-
+    
         socket.send(JSON.stringify({ action: 'join', username: localUserName, room: roomName }));
     }
 
     // Group chat creation event handlers
-    $('#createGroupChatBtn').click(function() {
-        $('#createGroupChatModal').modal('show');
-    });
-
     $('#createGroupBtn').click(function() {
         var groupName = $('#groupName').val().trim();
         var groupUsers = $('#groupUsers').val().trim().split(',').map(user => user.trim());
+        var localUserName = localStorage.getItem('username'); // Get the current user's username
+    
         if (groupName && groupUsers.length > 0) {
+            if (!groupUsers.includes(localUserName)) {
+                groupUsers.push(localUserName); // Add the current user to the list
+            }
             var room = generateRoomName(groupName, ...groupUsers);
-
+    
             console.log(`Creating group chat with name: ${groupName}, Users: ${groupUsers}`);
             // Send room creation request to the Flask server to save it in the database
             $.ajax({
@@ -190,33 +254,37 @@ $(document).ready(function() {
                     $('#createGroupChatModal').modal('hide');
                 },
                 error: function(xhr, status, error) {
-                    console.error('Room creation failed', xhr.responseJSON.message);
+                    console.error('Room creation failed', xhr.responseJSON ? xhr.responseJSON.message : error);
                 }
             });
         } else {
             alert('Please enter a group name and at least one user.');
         }
     });
+    
 
     // Search for rooms
     $('#searchRoomInput').on('input', function() {
         var query = $(this).val();
+        var token = localStorage.getItem('token'); // Get the token from localStorage
         if (query.trim() !== '') {
             $.ajax({
                 url: 'http://localhost:5000/search_rooms',
                 type: 'GET',
-                data: { query: query },
+                data: { query: query, token: token },
                 success: function(response) {
+                    console.log('Search rooms response:', response); // Debugging log
                     displayRoomSearchResults(response);
                 },
                 error: function(xhr, status, error) {
-                    console.error('Search failed', xhr.responseJSON.message);
+                    console.error('Search failed', xhr.responseJSON ? xhr.responseJSON.message : error);
                 }
             });
         } else {
             $('#roomSearchResults').empty();
         }
     });
+    
 
     function displayRoomSearchResults(rooms) {
         var results = $('#roomSearchResults');
@@ -234,6 +302,10 @@ $(document).ready(function() {
             var roomName = $(this).data('name');
             console.log(`Room ${roomName} clicked for joining.`);
             joinRoom(roomName);
+            
+            // Hide search results and clear the search input
+            $('#searchRoomInput').val('');
+            $('#roomSearchResults').empty();
         });
     }
 
